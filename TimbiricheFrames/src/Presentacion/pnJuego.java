af017b4ab701bc57fde08fca2cde6c0d8a766933
IsaiPerez <alfredo.perez216714@potros.itson.edu.mx>
2021-09-29 18:05:14 -0700

    A- 29/9/21 A-F 1.0.1.6

--- TimbiricheFrames/src/Presentacion/pnJuego.java
+++ TimbiricheFrames/src/Presentacion/pnJuego.java
@@ -3,21 +3,25 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package Presentacion;
 
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.geom.Ellipse2D;
+import java.util.ArrayList;
 
 /**
  *
  * @author crist
  */
 public class pnJuego extends javax.swing.JPanel {
-    private final int tamanio;
 
+    private final int tamanio;
+    private ArrayList<Punto> puntosList = new ArrayList();
+    private int pulsacion=1;
+    Punto p1;
+    Punto p2;
     public pnJuego(int tamanio) {
         initComponents();
         this.tamanio = tamanio;
@@ -28,28 +32,36 @@ public class pnJuego extends javax.swing.JPanel {
     public void paintComponent(Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
         super.paintComponent(g);
-
-        g2d.setPaint(Color.BLUE);
+        Punto punto;
+        g2d.setPaint(Color.BLACK);
 
         int posicion_x = 15;
         int posicion_y = 15;
         for (int i = 0; i < tamanio; i++) {
             for (int j = 0; j < tamanio; j++) {
                 Ellipse2D e1 = new Ellipse2D.Double(posicion_x, posicion_y, 15, 15);
+                punto = new Punto(posicion_x, posicion_y, 15, 15);
+                puntosList.add(punto);
                 g2d.fill(e1);
-                posicion_y += 50;
-            }
-            posicion_y = 15;
-            posicion_x += 50;
-        }
+                posicion_x += 50;
 
+            }
+            posicion_x = 15;
+            posicion_y += 50;
+        }
     }
-    
+
+    public void dibujarLinea() {
+        for (Punto punto : puntosList) {
+            
+        }        
+    }
+
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        setBackground(new java.awt.Color(255, 255, 255));
+        setBackground(new java.awt.Color(153, 153, 153));
         addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
                 formMouseClicked(evt);
@@ -69,11 +81,16 @@ public class pnJuego extends javax.swing.JPanel {
     }// </editor-fold>//GEN-END:initComponents
 
     private void formMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_formMouseClicked
-        // TODO add your handling code here:
+        if (pulsacion == 1 ) {
+            p1 = new Punto(evt.getX(), evt.getY(), 15, 0);
+            pulsacion++;
+        }else{
+            p2 = new Punto(evt.getX(), evt.getY(), 15, 0);
+            pulsacion--;
+        }
     }//GEN-LAST:event_formMouseClicked
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     // End of variables declaration//GEN-END:variables
-
 }
