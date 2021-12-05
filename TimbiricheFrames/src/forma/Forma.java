64021c3ec970a3ae47f95f315501f4a26dafbc02
IsaiPerez <alfredo.perez216714@potros.itson.edu.mx>
2021-12-04 20:42:15 -0700

    04/12/2021
    Se agrega la clase abstracta de forma y las clases que las extienden

---
+++ TimbiricheFrames/src/forma/Forma.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package forma;
+
+/**
+ *
+ * @author crist
+ */
+public abstract class Forma {
+    private float x;
+    private float y;
+    
+    public abstract void dibujar();
+}
